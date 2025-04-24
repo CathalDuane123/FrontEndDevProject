40f733c77c2b575cf74c528ee29b4f16d463fa4e
unknown <G00430973@atu.ie>
2025-04-24 21:48:58 +0000

    Attempt at Storage

--- src/app/ticket/ticket.page.ts
+++ src/app/ticket/ticket.page.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, 
+  IonItem, IonLabel, IonList, IonRadio, IonRadioGroup, IonButton } from '@ionic/angular/standalone';
+//import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router';
 
-@Component({
+@Component
+({
   selector: 'app-ticket',
   templateUrl: './ticket.page.html',
   styleUrls: ['./ticket.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
-export class TicketPage implements OnInit {
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, 
+    IonBackButton, IonButtons, IonItem, IonLabel, IonList, IonRadio, IonRadioGroup, 
+    IonButton]
+  })
+export class TicketPage implements OnInit 
+{
+  ticket:number = 1;
 
-  constructor() { }
-
+  //constructor(private storage:Storage, private router:Router) { }
+  //constructor(private storage:Storage){ }
+  constructor(private router:Router){ }
   ngOnInit() {
   }
+/*
+  async onButtonClick() 
+  {
+    //console.log("Status: " + this.status);
+    await this.storage.create();
+    await this.storage.set('status', this.ticket);
+    this.router.navigateByUrl('/home')
+  }
+
+  async ionViewWillEnter()
+  {
+    await this.storage.create();
+    this.ticket = await this.storage.get('stickrt');
+  }*/
 
 }
